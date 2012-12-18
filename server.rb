aae207ece00084307fac3cc980b55f4e51c93f34
Juliana Lucena <julianalucenaa@gmail.com>
2012-12-18 15:21:03 -0300

    Adds Space crossfilter.

--- server.rb
+++ server.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
-  "Hello reports!"
+  erb :space_filter
+end
+
+get '/space-stats.json' do
+  content_type :json
+  file = File.read('views/space-stats.json')
 end
