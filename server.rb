6ce73d99d6d60586844ab53c56e0be5ade0f84d9
Juliana Lucena <julianalucenaa@gmail.com>
2012-12-18 15:40:12 -0300

    Adds treemap-zoomable.

--- server.rb
+++ server.rb
@@ -5,7 +5,16 @@ get '/' do
   erb :space_filter
 end
 
+get '/treemap-zoom' do
+  erb :treemap_zoom
+end
+
 get '/space-stats.json' do
   content_type :json
   file = File.read('views/space-stats.json')
+end
+
+get '/data/flare-students.json' do
+  content_type :json
+  file = File.read('views/flare-students.json')
 end
